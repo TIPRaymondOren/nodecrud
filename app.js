d4268daf04db5fc1e11c9c5ffd45d56c86d2a250
TIPRaymondOren <mrjoren@tip.edu.ph>
2024-05-05 01:33:41 +0800

    Revert "Test commit"
    This reverts commit d48dd360f1c6f3f7dc1eb0c83390d384de17685d.

--- app.js
+++ app.js
@@ -39,9 +39,9 @@ app.use('/crud', deleteRouter);
 
 
 // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
 // error handler
 app.use(function(err, req, res, next) {
--- models/read-model.js
+++ models/read-model.js
@@ -1,10 +1,11 @@
-var db = require('../database');
-module.exports = {
-  readData: function (callback) {
-    var sql = 'SELECT * FROM crud';
+var db=require('../database');
+module.exports={
+  readData:function(callback){
+    var sql='SELECT * FROM crud';
     db.query(sql, function (err, data, fields) {
-      if (err) throw err;
-      return callback(data);
-    });
-  },
-};
+    if (err) throw err;
+    return callback(data);
+    });  
+  }
+ 
+}
