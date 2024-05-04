d48dd360f1c6f3f7dc1eb0c83390d384de17685d
TIPRaymondOren <mrjoren@tip.edu.ph>
2024-05-05 01:33:22 +0800

    Test commit

--- models/read-model.js
+++ models/read-model.js
@@ -1,11 +1,10 @@
-var db=require('../database');
-module.exports={
-  readData:function(callback){
-    var sql='SELECT * FROM crud';
+var db = require('../database');
+module.exports = {
+  readData: function (callback) {
+    var sql = 'SELECT * FROM crud';
     db.query(sql, function (err, data, fields) {
-    if (err) throw err;
-    return callback(data);
-    });  
-  }
- 
-}
+      if (err) throw err;
+      return callback(data);
+    });
+  },
+};
